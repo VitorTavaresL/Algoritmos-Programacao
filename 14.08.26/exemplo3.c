ed4688c319c7b315ad4b62618412368e6f466bae
VITOR TAVARES LEITE <vitor.tl1@puccampinas.edu.br>
2026-08-14 08:37:55 -0300

    refractor: exemplo3.c

---
+++ 14.08.26/exemplo3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+    int a = 3, b = 5;
+    printf("a = %d\n", a);
+    printf("b = %d\n", b);
+    printf("a + b = %d\n", a + b);
+    printf("a - b = %d\n", a - b);
+    printf("a / b = %d\n", a / b);
+    printf("a * b = %d\n", a * b);
+    return 0;
+}
