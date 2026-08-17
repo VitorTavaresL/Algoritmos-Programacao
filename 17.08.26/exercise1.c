d44e77f3c1874f503c9c527bcf9dfc0d0edf9263
Vitor <vitor.tavares.leite@gmail.com>
2026-08-17 10:57:48 -0300

    add: hipotenusa

--- 17.08.26/exercise1.c
+++ 17.08.26/exercise1.c
@@ -3,12 +3,13 @@
 
 int main(){
     float a, b;
-    double h;
+    double h, hipotenusa;
 
     printf("insira os valores dos catetos: ");
     scanf("%f %f", &a, &b);
 
     h = pow(a, 2) + pow(b, 2);
+    hipotenusa = sqrt(h);
     printf("a hipotenusa é igual a: %lf", h);
 
 }
