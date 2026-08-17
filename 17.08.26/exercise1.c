c976df7e5bafae32b351599d55e12d360d5bc19e
Vitor <vitor.tavares.leite@gmail.com>
2026-08-17 11:11:52 -0300

    optimization

--- 17.08.26/exercise1.c
+++ 17.08.26/exercise1.c
@@ -3,15 +3,14 @@
 
 int main(){
     float a, b;
-    double h, hipotenusa;
+    double h;
 
     printf("insira os valores dos catetos: ");
     scanf("%f %f", &a, &b);
 
     h = pow(a, 2) + pow(b, 2);
-    hipotenusa = sqrt(h);
-
-    printf("a hipotenusa é igual a: %lf", hipotenusa);
+    
+    printf("a hipotenusa é igual a: %lf", sqrt(h));
     
     return 0;
 }
