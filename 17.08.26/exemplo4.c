61769633353ca0d8b4709555074b09f49fb7e2ba
VITOR TAVARES LEITE <vitor.tl1@puccampinas.edu.br>
2026-08-17 10:37:16 -0300

    add: 17.08.26

---
+++ 17.08.26/exemplo4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+    int a, b;
+    printf("entre com dois numeros inteiros: ");
+    scanf("%d %d", &a, &b);
+    printf("a = %d\n", a);
+    printf("b = %d\n", b);
+    printf("a = %p\n", &a);
+    printf("b = %p\n", &b);
+    printf("a + b = %d\n", a + b);
+    printf("a - b = %d\n", a - b);
+    printf("a / b = %d\n", a / b);
+    printf("a * b = %f\n", (float)a * b);
+    printf("a %% b = %d\n", a % b );
+    return 0;
+}
